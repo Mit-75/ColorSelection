0066aab89f5985f3a572dfe605bd9631c2819595
Dmitry Parkhomenko <oku_74@mail.ru>
2024-03-04 21:39:10 +0500

    - Создана ветка для разработки%     - Development

--- ColorSelection/ContentView.swift
+++ ColorSelection/ContentView.swift
@@ -8,19 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var redSliderValue: Double = 40
-    @State private var greenSliderValue: Double = 120
-    @State private var blueSliderValue: Double = 60
+    @State private var redSliderValue = Double.random(in: 0...255)
+    @State private var greenSliderValue = Double.random(in: 0...255)
+    @State private var blueSliderValue = Double.random(in: 0...255)
     
     var body: some View {
         ZStack{
             Color(.init(red: 0.1, green: 0.4, blue: 0.5))
                 .ignoresSafeArea()
-            VStack(spacing: 50) {
-                Color.init(red: redSliderValue / 255, green: greenSliderValue / 255, blue: blueSliderValue / 255)
+            VStack(spacing: 60) {
+                Color.init(
+                    red: redSliderValue / 255,
+                    green: greenSliderValue / 255,
+                    blue: blueSliderValue / 255
+                )
                     .frame(height: 170)
                     .clipShape(.rect(cornerRadius: 20))
-                    .overlay(RoundedRectangle(cornerRadius: 20).stroke(.white, lineWidth: 5))
+                    .overlay(RoundedRectangle(cornerRadius: 20)
+                        .stroke(.white, lineWidth: 5))
                 VStack {
                     RedSlidersView(sliderValue: $redSliderValue)
                         .tint(.red)
@@ -31,7 +36,7 @@ struct ContentView: View {
                 }
                 Spacer()
             }
-            .padding(20)
+            .padding(10)
         }
     }
 }
@@ -42,16 +47,20 @@ struct ContentView: View {
 
 struct RedSlidersView: View {
     @Binding var sliderValue: Double
-    
+    @State private var valueTF: String = "123"
     var body: some View {
         HStack() {
-            Text(sliderValue.formatted())
+            Text(lround(sliderValue).formatted())
                 .frame(width: 55)
                 .font(.system(.title3, design: .rounded, weight: .heavy))
                 .foregroundStyle(.white)
-                .overlay(RoundedRectangle(cornerRadius: 10).stroke(.white, lineWidth: 2))
+                .overlay(RoundedRectangle(cornerRadius: 10)
+                    .stroke(.white, lineWidth: 1))
             Slider(value: $sliderValue, in: 1...255, step: 1)
-                //.tint(.red)
+            TextField("", text: $valueTF)
+                .textFieldStyle(.roundedBorder)
+                .font(.system(.title3, design: .rounded, weight: .heavy))
+                .frame(width: 55, height: 40)
         }
     }
 }
