06c1b86009e6eaa043f6c6d8e3a6edcca457be4e
Dmitry Parkhomenko <oku_74@mail.ru>
2024-03-04 21:40:56 +0500

    Подкорректировал расстановку кода

--- ColorSelection/ContentView.swift
+++ ColorSelection/ContentView.swift
@@ -17,10 +17,15 @@ struct ContentView: View {
             Color(.init(red: 0.1, green: 0.4, blue: 0.5))
                 .ignoresSafeArea()
             VStack(spacing: 50) {
-                Color.init(red: redSliderValue / 255, green: greenSliderValue / 255, blue: blueSliderValue / 255)
+                Color.init(
+                    red: redSliderValue / 255,
+                    green: greenSliderValue / 255,
+                    blue: blueSliderValue / 255
+                )
                     .frame(height: 170)
                     .clipShape(.rect(cornerRadius: 20))
-                    .overlay(RoundedRectangle(cornerRadius: 20).stroke(.white, lineWidth: 5))
+                    .overlay(RoundedRectangle(cornerRadius: 20)
+                        .stroke(.white, lineWidth: 5))
                 VStack {
                     RedSlidersView(sliderValue: $redSliderValue)
                         .tint(.red)
@@ -49,9 +54,9 @@ struct RedSlidersView: View {
                 .frame(width: 55)
                 .font(.system(.title3, design: .rounded, weight: .heavy))
                 .foregroundStyle(.white)
-                .overlay(RoundedRectangle(cornerRadius: 10).stroke(.white, lineWidth: 2))
+                .overlay(RoundedRectangle(cornerRadius: 10)
+                    .stroke(.white, lineWidth: 2))
             Slider(value: $sliderValue, in: 1...255, step: 1)
-                //.tint(.red)
         }
     }
 }
