d49fa1818c8124537973e68de2190c507a7eded9
Dmitry Parkhomenko <oku_74@mail.ru>
2024-03-04 11:48:10 +0500

    Initial Commit

---
+++ ColorSelection/ColorSelectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSelectionApp.swift
+//  ColorSelection
+//
+//  Created by Dmitry Parhomenko on 04.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ColorSelectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
