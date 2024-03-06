f3ec7f1b7f4e70af48f3e377ba32ba04d2b1730a
Dmitry Parkhomenko <oku_74@mail.ru>
2024-03-06 10:25:57 +0500

    - неполучается связать перенос данных из TextField в поле Теxt(displayText) - не соображу как при запуске программы в поле  TextField  установить значения Slider

--- ColorSelection/ContentView.swift
+++ ColorSelection/ContentView.swift
@@ -12,31 +12,41 @@ struct ContentView: View {
     @State private var greenSliderValue = Double.random(in: 0...255)
     @State private var blueSliderValue = Double.random(in: 0...255)
     
+    @State private var displayText = "###"
+    
     var body: some View {
         ZStack{
             Color(.init(red: 0.1, green: 0.4, blue: 0.5))
                 .ignoresSafeArea()
-            VStack(spacing: 60) {
+            VStack(spacing: 40) {
                 Color.init(
                     red: redSliderValue / 255,
                     green: greenSliderValue / 255,
                     blue: blueSliderValue / 255
                 )
-                    .frame(height: 170)
-                    .clipShape(.rect(cornerRadius: 20))
-                    .overlay(RoundedRectangle(cornerRadius: 20)
-                        .stroke(.white, lineWidth: 5))
+                .frame(height: 170)
+                .clipShape(.rect(cornerRadius: 20))
+                .overlay(RoundedRectangle(cornerRadius: 20)
+                    .stroke(.white, lineWidth: 5))
+                Text(displayText)
+                    .frame(width: 300, height: 45)
+                    .font(.system(.title3, design: .rounded, weight: .heavy))
+                    .foregroundStyle(.white)
+                    .overlay(RoundedRectangle(cornerRadius: 10)
+                        .stroke(.white, lineWidth: 1))
+                    
                 VStack {
-                    RedSlidersView(sliderValue: $redSliderValue)
+                    SlidersView(sliderValue: $redSliderValue)
                         .tint(.red)
-                    RedSlidersView(sliderValue: $greenSliderValue)
+                    SlidersView(sliderValue: $greenSliderValue)
                         .tint(.green)
-                    RedSlidersView(sliderValue: $blueSliderValue)
+                    SlidersView(sliderValue: $blueSliderValue)
                         .tint(.blue)
+                    ButtonView()
                 }
                 Spacer()
             }
-            .padding(10)
+            .padding(15)
         }
     }
 }
@@ -45,9 +55,9 @@ struct ContentView: View {
     ContentView()
 }
 
-struct RedSlidersView: View {
+struct SlidersView: View {
     @Binding var sliderValue: Double
-    @State private var valueTF: String = "123"
+    @State private var valueTF: String = ""
     var body: some View {
         HStack() {
             Text(lround(sliderValue).formatted())
@@ -56,11 +66,39 @@ struct RedSlidersView: View {
                 .foregroundStyle(.white)
                 .overlay(RoundedRectangle(cornerRadius: 10)
                     .stroke(.white, lineWidth: 1))
-            Slider(value: $sliderValue, in: 1...255, step: 1)
+            Slider(
+                value: $sliderValue,
+                in: 1...255,
+                step: 1) { editing in
+                    valueTF = lround(sliderValue).formatted()
+                }
             TextField("", text: $valueTF)
                 .textFieldStyle(.roundedBorder)
                 .font(.system(.title3, design: .rounded, weight: .heavy))
                 .frame(width: 55, height: 40)
         }
     }
+    private func getDisplayText() {
+        
+    }
+}
+
+struct ButtonView: View {
+    var body: some View {
+        Button(action: {})  {
+            Text("Done")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(.white)
+            
+        }
+        .frame(width: 100, height: 45)
+        .background(.blue)
+        .clipShape(.rect(cornerRadius: 10))
+        .overlay(RoundedRectangle(cornerRadius: 10)
+            .stroke(.white, lineWidth: 3))
+        .shadow(radius: 10)
+        .padding(25)
+    }
+    
 }
